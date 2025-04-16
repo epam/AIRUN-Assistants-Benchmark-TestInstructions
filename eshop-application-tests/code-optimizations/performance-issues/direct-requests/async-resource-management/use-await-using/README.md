e580377782219ff2d4e1d10187bf683df0b0d122
Alexander Tarvid <atarvid@inbox.lv>
2025-04-16 16:37:08 +0300

    First implementation

---
+++ eshop-application-tests/code-optimizations/performance-issues/direct-requests/async-resource-management/use-await-using/README.md
@@ -0,0 +1,29 @@
+# Use await using instead of using in asynchronous methods
+
+## Act
+
+- Open the eShop application:
+<https://github.com/AlexanderTarvid/eShop-250408>
+
+<details>
+<summary>Chat IDE:</summary>
+
+- Open file **src/OrderProcessor/Services/GracePeriodManagerService.cs**
+- Open the chat AI interface and enter:
+
+```text
+Use `await using` in all async methods of the current file.
+```
+
+- Submit the request
+- Accept the suggestions
+- Save the file
+
+</details>
+
+## Assert
+
+- There are no compiler errors
+- Copy **validator.py** and **validator_utils.py** to **src/OrderProcessor/Services/**, open that folder in PowerShell and execute **validator.py**. Observe that the validator returns **Validation passed**.
+
+The **\*.cs** files with the expected implementation of the two functions that should be added or modified by AI are included here for reference.
---
+++ eshop-application-tests/code-optimizations/performance-issues/direct-requests/async-resource-management/use-await-using/validator.py
@@ -0,0 +1,60 @@
+import os
+import re
+import sys
+import validator_utils
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
+code_file_path = './GracePeriodManagerService.cs'
+
+
+def phrase_present(line: str, word: str) -> bool:
+    pattern = r'\b{}\b'.format(re.escape(word))
+    return bool(re.search(pattern, line))
+
+
+def validate(code: str) -> bool:
+    validation_failed = False
+
+    # extracting method GetConfirmedGracePeriodOrders (it's the only applicable for this test)
+    getConfirmedGracePeriodOrders_method = validator_utils.extract_csharp_method(code, 'GetConfirmedGracePeriodOrders')
+    if getConfirmedGracePeriodOrders_method is None:
+        print(f'{RED}Cannot find method GetConfirmedGracePeriodOrders.{RESET}')
+        validation_failed = validation_failed or True
+    else:
+        validator_utils.clean_extra_whitespace(getConfirmedGracePeriodOrders_method)
+        lines = getConfirmedGracePeriodOrders_method.splitlines()
+        for line in lines:
+            if phrase_present('using') and not phrase_present('await using'):
+                print(f'{RED}using not changed to await using on the following line:{RESET}')
+                print(f'{RED}  {line}{RESET}')
+                validation_failed = True
+    
+    return not validation_failed
+
+
+# checking that the files exist
+if (not os.path.exists(code_file_path)):
+    print(f"Error: The code file ('{code_file_path}') not found")
+    sys.exit(1)
+
+# loading C# code
+try:
+    with open(code_file_path) as code_file:
+        full_code = code_file.read()
+except Exception as e:
+    print(f"Error reading {code_file_path}: {e}")
+    sys.exit(1)
+
+# validating
+validation_passed = validate(full_code)
+
+# outputting validation results
+if validation_passed:
+    print(f'{GREEN}Validation passed{RESET}')
+    sys.exit(0)
+else:
+    print(f'{RED}Validation failed{RESET}')
+    sys.exit(1)
